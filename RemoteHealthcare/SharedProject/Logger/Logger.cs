8a744545be8420be1321b724ccc27ad6e54faded
Merijn <mfg.gommeren@student.avans.nl>
2022-09-26 10:55:45 +0200

    Fixed Logger
    Co-Authored-by: Jasper Hopmans <jasperhopmans@gmail.com>

---
+++ RemoteHealthcare/SharedProject/Logger/Logger.cs
@@ -0,0 +1,91 @@
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ServerApplication.Log
+{
+    public class Logger
+    {
+        public static LogLevel PrintLevel = LogLevel.All;
+        
+        #region default code for it to work (internet)
+        private const int StdOutputHandle = -11;
+        private const uint EnableVirtualTerminalProcessing = 0x0004;
+        private const uint DisableNewlineAutoReturn = 0x0008;
+
+        [DllImport("kernel32.dll")]
+        private static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetLastError();
+        #endregion
+
+        public static void LogMessage(LogImportance logImportance, string message, Exception? exception = null)
+        {
+            if (logImportance.Level > (int) PrintLevel)
+                return;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var iStdOut = GetStdHandle(StdOutputHandle);
+                _ = GetConsoleMode(iStdOut, out var outConsoleMode) && SetConsoleMode(iStdOut, outConsoleMode | EnableVirtualTerminalProcessing);
+            
+            }
+            var builder = new StringBuilder();
+        
+            //Buildup first line
+            CreatePrefix(logImportance, builder);
+            builder.Append(logImportance.ColorCode + message);
+
+            if (exception != null)
+            {
+                var ex = exception;
+                while (ex != null)
+                {
+                    builder.AppendLine();
+                    builder.Append("   " + LogColor.Yellow.Color);
+                    builder.Append(exception.GetType().Name.Replace("Exception", string.Empty));
+                    builder.Append(LogColor.Gray.Color + ": " + LogColor.Blue + exception.Message);
+                    ex = ex.InnerException;
+                }
+
+                if (exception.StackTrace != null)
+                {
+                    var stackTraceElements = exception.StackTrace.Split('\n').Select(x => x.Trim()).ToList();
+                    foreach (var stackTraceElement in stackTraceElements)
+                    {
+                        builder.AppendLine();
+                        builder.Append(LogColor.Gray.Color + "     ");
+                        builder.Append(stackTraceElement);
+                    }
+                }
+            }
+
+            builder.Append(LogColor.White.Color);
+            Console.WriteLine(builder);
+
+        }
+
+        private static void CreatePrefix(LogImportance logImportance, StringBuilder builder)
+        {
+            builder.Append(LogColor.Gray.Color + "[" + logImportance.ColorCode + logImportance.Name +  LogColor.Gray.Color + "] ");
+        }
+    
+    }
+
+    public enum LogLevel : ushort
+    {
+        All = 6,
+        Debug = 4,
+        Information = 5,
+        Warning = 3,
+        Error = 2, 
+        Fatal = 1,
+        Off = 0,
+    
+    }
+}

0684a8cb56631c3ab5a5880021df23bbf26f48d9
Oliver Norton <o.k.norton21@gmail.com>
2022-10-05 18:50:48 +0200

    First Working Variable for Binding setup

---
+++ RemoteHealthcare/DoctorApplication/MVVM/Model/UserDataModel.cs
@@ -0,0 +1,42 @@
+﻿using DoctorApplication.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorApplication.MVVM.Model
+{
+    public class UserDataModel : INotifyPropertyChanged
+    {
+        //userdata
+        private string userName;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { 
+                userName = value;
+                OnPropertyChanged(nameof(UserName));
+            }
+        }
+
+
+        public UserDataModel()
+        {
+            this.UserName = "Test";
+        }
+
+       
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
--- RemoteHealthcare/DoctorApplication/MVVM/ViewModel/DataViewModel.cs
+++ RemoteHealthcare/DoctorApplication/MVVM/ViewModel/DataViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoctorApplication.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DoctorApplication.MVVM.ViewModel
 {
-    internal class DataViewModel
+    internal class DataViewModel : ObservableObject
     {
     }
 }

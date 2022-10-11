03a4663deac201930f2a2a37d3f813752a338cec
Oliver Norton <o.k.norton21@gmail.com>
2022-10-11 12:05:20 +0200

    Added emergency button
    Not functional

--- RemoteHealthcare/DoctorApplication/MVVM/Model/ArrayList.cs
+++
@@ -1,7 +0,0 @@
-﻿/* This is the model for the userdata. It contains all the data that is currently needed to be displayed. */
-namespace DoctorApplication.MVVM.Model
-{
-    internal class ArrayList<T>
-    {
-    }
-}

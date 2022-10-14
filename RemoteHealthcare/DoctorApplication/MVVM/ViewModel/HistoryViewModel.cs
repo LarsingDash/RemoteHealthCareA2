d30cd4d1cd9e889f8ccb066ddd6a9575a89421bf
Oliver Norton <o.k.norton21@gmail.com>
2022-10-14 12:26:05 +0200

    HistoryScreenImplementation V1

---
+++ RemoteHealthcare/DoctorApplication/MVVM/ViewModel/HistoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorApplication.MVVM.ViewModel
+{
+    internal class HistoryViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}

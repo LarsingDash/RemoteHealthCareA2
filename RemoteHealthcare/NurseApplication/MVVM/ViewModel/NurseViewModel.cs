757efa1a0d6cc52cd8721eabacf8f4d9e03b5ae9
Oliver Norton <o.k.norton21@gmail.com>
2022-10-23 18:35:56 +0200

    Fixed display Issue and added notification sound

--- RemoteHealthcare/NurseApplication/MVVM/ViewModel/NurseViewModel.cs
+++ RemoteHealthcare/NurseApplication/MVVM/ViewModel/NurseViewModel.cs
@@ -6,21 +6,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using NurseApplication.MVVM.Model;
+using System.Media;
+using Caliburn.Micro;
 
 namespace NurseApplication.MVVM.ViewModel
 {
     internal class NurseViewModel : ObservableObject
     {
-        public ObservableCollection<AlertModel> Alerts;
+        public BindableCollection<AlertModel> Alerts { get; set; }
 
         public NurseViewModel()
         {
-           Alerts = new ObservableCollection<AlertModel>();
+           Alerts = new BindableCollection<AlertModel>();
+            AddAlert("test", "123124");
+            Console.WriteLine(Alerts.Count);
         }
 
         public void AddAlert(string userName, string bikeId)
         {
             Alerts.Add(new AlertModel(userName, bikeId));
+            SystemSounds.Beep.Play();
         }
     }
 }

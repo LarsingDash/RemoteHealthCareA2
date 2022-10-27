474fbde399c4706c1991a8b4fab7258096a9f1ce
Merijn <mfg.gommeren@student.avans.nl>
2022-10-27 11:07:37 +0200

    When doctor disconnetcs, server stops spamming messages to them

--- RemoteHealthcare/ServerApplication/Client/DataHandlers/CommandHandlers/ChangeData.cs
+++ RemoteHealthcare/ServerApplication/Client/DataHandlers/CommandHandlers/ChangeData.cs
@@ -52,7 +52,14 @@ public class ChangeData : CommandHandler
                 CheckValueInData(ob, message, "heartrate", startTime);
                 foreach (var clientData in server.SubscribedSessions[uuid])
                 {
-                    clientData.SendEncryptedData(message.ToString());
+                    if (server.users.Contains(clientData))
+                    {
+                        clientData.SendEncryptedData(message.ToString());
+                    }
+                    else
+                    {
+                        server.SubscribedSessions[uuid].Remove(clientData);
+                    }
                 }
             }
             data.SendEncryptedData(JsonFileReader.GetObjectAsString("ErrorResponse",new Dictionary<string, string>()

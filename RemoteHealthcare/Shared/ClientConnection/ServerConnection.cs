dbce8e9372378159ffea266560aafa98d928594d
Jasper Hopmans <Jasperhopmans@gmail.com>
2022-10-31 21:41:42 +0100

    ServerConnection for demonstration

--- RemoteHealthcare/Shared/ClientConnection/ServerConnection.cs
+++ RemoteHealthcare/Shared/ClientConnection/ServerConnection.cs
@@ -2,7 +2,7 @@ namespace Shared;
 
 public class ServerConnection
 {
-    public static string Hostname = "localhost";
+    public static string Hostname = "145.49.27.26";
     public static int Port = 2460;
     
 }
